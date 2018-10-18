98b18ac8066269509ab23b5cb4210d71209461b4
NaJiPeng <2462397890@qq.com>
2018-10-18 22:25:04 +0800

    添加zxing二维码

--- app/src/main/java/com/njp/smartlab/MyApplication.kt
+++ app/src/main/java/com/njp/smartlab/MyApplication.kt
@@ -5,6 +5,7 @@ import com.kingja.loadsir.core.LoadSir
 import com.njp.smartlab.utils.loadsir.FailCallback
 import com.njp.smartlab.utils.loadsir.LoadingCallback
 import com.tencent.mmkv.MMKV
+import com.uuzuche.lib_zxing.activity.ZXingLibrary
 
 /**
  * Application实例
@@ -20,6 +21,8 @@ class MyApplication : Application() {
         instance = this
 
         MMKV.initialize(this)
+
+        ZXingLibrary.initDisplayOpinion(this)
 
         LoadSir.beginBuilder()
                 .addCallback(LoadingCallback())
